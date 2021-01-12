8565656c700d6908634f2b42c9fb5b317ed20784
Victoria Schoeffel <vns28cam.ac.uk>
2021-01-12 18:07:13 +0100

    Used case statement instead of if statement, and defined method for promp

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -3,29 +3,33 @@
 # perform the operation on the two numbers
 # output the result
 
-Kernel.puts ("Welcome to your Calculator!")
-
-Kernel.puts "Please give me your first number"
-number_1 = Kernel.gets().chomp()
-
-Kernel.puts "Please give me your second number."
-number_2 = Kernel.gets().chomp()
-
-Kernel.puts "What operation would you like to perform on these numbers? (Addition, Subtraction, Multiplication, Division"
-operation = Kernel.gets().chomp().to_s()
-
-if operation == "addition" || operation == "Addition"
-  result = number_1.to_i() + number_2.to_i()
-elsif operation == "subtraction" || operation == "Subtraction"
-  result = number_1.to_i() - number_2.to_i()
-elsif operation == "multiplication" || operation == "Multiplication"
-  result = number_1.to_i() * number_2.to_i() 
-elsif (operation == "division" || operation == "Division") && number_2 != 0 
-  result = number_1.to_f() / number_2.to_f()
-elsif operation == "division" && number_2.to_f() == 0
-  Kernel.puts "You can't divide by zero!"
-else
-  Kernel.puts "That is not a valid operation."
+def prompt(message)
+    Kernel.puts("=> #{message}")
 end
 
-Kernel.puts "The result is #{result}."
+prompt("Welcome to your Calculator!")
+prompt("Please give me your first number")
+number_1 = Kernel.gets().chomp()
+
+prompt("Please give me your second number.")
+number_2 = Kernel.gets().chomp()
+
+prompt("What operation would you like to perform on these numbers? (Addition, Subtraction, Multiplication, Division)")
+operation = Kernel.gets().chomp().to_s()
+
+
+result =  case operation
+          when "Addition"
+            number_1.to_i() + number_2.to_i()
+          when "Subtraction"
+            number_1.to_i() - number_2.to_i()
+          when operation == "Multiplication"
+            number_1.to_i() * number_2.to_i() 
+          when "Division"
+            number_1.to_f() / number_2.to_f()
+          else
+            prompt("That is not a valid operation.")
+          end
+
+prompt("The result is #{result}.") 
+
