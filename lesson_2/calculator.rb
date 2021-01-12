600614ec4dbf08a7bc6792b3c421551ce4b4e24d
Victoria Schoeffel <vns28cam.ac.uk>
2021-01-12 18:32:10 +0100

    Changes to calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -7,29 +7,69 @@ def prompt(message)
     Kernel.puts("=> #{message}")
 end
 
-prompt("Welcome to your Calculator!")
-prompt("Please give me your first number")
-number_1 = Kernel.gets().chomp()
+def valid_number?(num)
+num.to_i != 0
+end
 
-prompt("Please give me your second number.")
-number_2 = Kernel.gets().chomp()
+prompt("Welcome to your Calculator! Enter your name:")
 
-prompt("What operation would you like to perform on these numbers? (Addition, Subtraction, Multiplication, Division)")
-operation = Kernel.gets().chomp().to_s()
+name = ''
+loop do 
+  name = Kernel.gets().chomp().capitalize!
+  if name.empty?()
+    prompt("That is not a valid name.")
+  else
+   break
+  end
+end
+
+prompt("Welcome #{name}!")
+
+loop do 
+  number_1 = ''
+  loop do 
+    prompt("Please give me your first number")
+    number_1 = Kernel.gets().chomp()
+    if valid_number?(number_1)
+      break
+    else
+      prompt("That is not a valid number.")
+    end
+  end 
+
+  number_2 = ''
+  loop do 
+    prompt("Please give me your second number")
+    number_2 = Kernel.gets().chomp()
+    if valid_number?(number_2)
+      break
+    else
+      prompt("That is not a valid number.")
+    end
+  end 
+
+  prompt("What operation would you like to perform on these numbers? (Addition, Subtraction, Multiplication, Division)")
+  operation = Kernel.gets().chomp().to_s().downcase()
 
 
-result =  case operation
-          when "Addition"
-            number_1.to_i() + number_2.to_i()
-          when "Subtraction"
-            number_1.to_i() - number_2.to_i()
-          when operation == "Multiplication"
-            number_1.to_i() * number_2.to_i() 
-          when "Division"
-            number_1.to_f() / number_2.to_f()
-          else
-            prompt("That is not a valid operation.")
-          end
+  result =  case operation
+            when "addition"
+              number_1.to_i() + number_2.to_i()
+            when "subtraction"
+              number_1.to_i() - number_2.to_i()
+            when operation == "multiplication"
+              number_1.to_i() * number_2.to_i() 
+            when "division"
+              number_1.to_f() / number_2.to_f()
+            else
+              prompt("That is not a valid operation.")
+            end
 
-prompt("The result is #{result}.") 
+  prompt("The result is #{result}.") 
+  
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
 
+prompt("Thank you for using the calculator.")
