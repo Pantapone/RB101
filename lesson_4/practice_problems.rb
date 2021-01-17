657e189c3de79f46b1078e95bc407943fe00ecc9
Victoria Schoeffel <vns28cam.ac.uk>
2021-01-17 10:58:42 +0100

    first set of practice problems added

---
+++ lesson_4/practice_problems.rb
@@ -0,0 +1,30 @@
+#1
+# -> [1, 2, 3] because hi is evaluated to "true", thus all elements of the array are returned
+
+#2
+# count counts how many elements in the array evaluate to "true" and returns the number of the times
+
+#3 
+# it returns an array for all the elements of the array which evaluate to "false"; as "puts num" evaluates to nil (which is not "truthy") [1, 2, 3] is returned in a new array
+
+#4
+# new hash: {"a" => "ant, ..."}
+
+#5
+# removes the first key, value pair
+
+#6
+# 11
+# pop returns the last element (caterpillar), while size gives the number of letters -> 1
+
+#7
+# the return value is the last expression (num.odd?) -> true ; any? will also return ture
+
+#8
+# returns the first specified elements form an array (aka here: 2) -> [1, 2] - it's not destructive 
+
+#9
+# [nil, "bear"] -> returns an array, nil as "ant" evaluates to false and that is not returned but the array returned is the same size as the original collection
+
+#10
+#[1, nil, nil] (nil because the if statement is true for the array value at index 1 & 2 and puts num always evaluates to nil)
