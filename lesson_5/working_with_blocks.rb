d4489211145508b6bfca3ec9113d66a65ebe2bb2
Victoria Schoeffel <vns28cam.ac.uk>
2021-01-18 10:54:41 +0100

    working with blocks article exercises added

---
+++ lesson_5/working_with_blocks.rb
@@ -0,0 +1,4 @@
+#Example3 -> [1, 3] as puts it not the return value anymore (puts .. returns nil), arr.first returns the integer in each case;  -> map transforms and returns the new array
+#Example4 -> 18, 7, 12 & [[18, 7], [3, 12]]
+#Example5 -> [[2, 4], [6,8]]
+#Example10 -> [[[2, 3], [4, 5]], [6, 7]]
