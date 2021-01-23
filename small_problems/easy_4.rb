d2c1817ac96b563271dd212328e0ec197f1c9338
Victoria Schoeffel <vns28cam.ac.uk>
2021-01-23 19:02:01 +0100

    parts of easy 4

--- small_problems/easy_4.rb
+++ small_problems/easy_4.rb
@@ -10,9 +10,37 @@ p short_long_short('', 'xyz') == "xyz"
 
 
 
-#2
-# def century(year)
 
+#2
+def century(year)
+  century = year / 100 + 1
+  century -= 1 if year % 100 == 0
+  
+  last_num = century % 10
+  if [11, 12, 13].include?(century % 100)
+    suffix = "th"
+  elsif century % 10 == 1
+    suffix = "st"
+  elsif century % 10 == 2
+    suffix = "nd"
+  elsif century % 10 == 3
+    suffix = "rd"
+  else
+    suffix = "th"
+  end
+
+  century.to_s + suffix
+end
+
+century(2000) == '20th'
+century(2001) == '21st'
+century(1965) == '20th'
+century(256) == '3rd'
+century(5) == '1st'
+century(10103) == '102nd'
+century(1052) == '11th'
+century(1127) == '12th'
+century(11201) == '113th'
 
 #3
 def leap_year?(year)
@@ -89,12 +117,43 @@ running_total([2, 5, 13]) == [2, 7, 20]
 running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
 running_total([3]) == [3]
 running_total([]) == []
+
+
 #7
 
+DIGITS = {
+  "0" => 0, "1" => 1, "2" => 2, "3" => 3, "4" => 4,
+  "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9
+}
 
+def string_to_integer(str)
+  chars = str.chars.map {|x| DIGITS[x]}
+  
+  value = 0
+  chars.each {|y| value = 10 * value + y}
+  value
+end
+
+string_to_integer('4321') == 4321
+string_to_integer('570') == 570
 
 #8
 
+def string_to_signed_integer(str)
+  case str[0]
+  when "+" then -string_to_integer(str[1..-1])
+  when "-" then string_to_integer(str[1..-1])
+  else string_to_integer(str)
+  end
+end
+
+p string_to_signed_integer('4321') == 4321
+p string_to_signed_integer('-570') == -570
+p string_to_signed_integer('+100') == 100
+
+
+
 #9
+
 
 #10
